0172cee1156ce76873c26ab670ee214e8ff48132
JalithaKanchana <it17059046@my.sliit.lk>
2021-03-09 19:21:43 +0530

    finall

--- bash_scripts/main_1.sh
+++ bash_scripts/main_1.sh
@@ -2,5 +2,5 @@
 
 echo "########### Run check_webserver_content.sh script and check_webserver_status.sh script ###########"
 
-ssh -i lseg  ec2-user@54.234.202.204 'bash -s' < check_webserver_content.sh 
-ssh -i lseg  ec2-user@54.234.202.204 'bash -s' < check_webserver_status.sh
+ssh -i lseg  ec2-user@54.197.194.240 'bash -s' < check_webserver_content.sh 
+ssh -i lseg  ec2-user@54.197.194.240 'bash -s' < check_webserver_status.sh
