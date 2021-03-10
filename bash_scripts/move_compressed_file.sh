b34cb74759aa7abf010672ab723fb4e0dcb2530d
JalithaKanchana <it17059046@my.sliit.lk>
2021-03-10 18:18:42 +0530

    final

--- bash_scripts/move_compressed_file.sh
+++ bash_scripts/move_compressed_file.sh
@@ -3,4 +3,4 @@
 now=`date +"%Y-%m-%d"`
 
 echo "###########copy files to admin server########"
-scp -i lseg ec2-user@54.197.194.240:/home/ec2-user/logs_content.$now.tar.gz .
+scp -i lseg ec2-user@:107.23.253.133/home/ec2-user/logs_content.$now.tar.gz .
